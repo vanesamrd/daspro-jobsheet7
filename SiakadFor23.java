d30a795247e17633c22e2dc4fbfc64d9f952158e
vanesamrd <paneja435@gmail.com>
2024-10-09 12:00:39 +0700

    feat : percobaan 1 + modifikasi

---
+++ SiakadFor23.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class SiakadFor23 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;  
+        double batasKelulusan = 60.0;   
+
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+            if (nilai >= batasKelulusan) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
+        }
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
+    }
+}
